4326ed5b5ba79e9b3ef61c08c4c054e280ffcc0b
awez78 <shaikhawez7178@gmail.com>
2026-03-02 11:50:03 +0530

    Enhance program header and implement factorial logic
    Added coder name and date to the program header. Implemented factorial calculation logic.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder: Shaikh Awez
+# Date: 02-02-2026
 
 print("--- Factorial Finder ---\n")
 
 
 # Write your code here
+num = int(input('Enter a number: '))
+og_num = num
+for a in range(1,num):
+    num = num*a
+print(f"Factorial of {og_num} is {num}")
